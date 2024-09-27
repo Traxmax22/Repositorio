ce702ab1e6b89bfa820609e371828e3cee9406cb
Traxmax22 <181796234+Traxmax22@users.noreply.github.com>
2024-09-26 20:28:49 -0500

    finish all dark-mode configuration

--- noche.js
+++ noche.js
@@ -1,3 +1,17 @@
 function modoOscuro() {
     document.getElementById("body").classList.add("oscuro");
+    document.getElementById("nav").classList.("dark");
+    document.getElementById("footer").classList.("dark");
+    document.getElementById("enlace").classList.add("enlace-oscuro");
+    document.getElementById("input").classList.add("dark");
+    document.getElementById("input2").classList.add("dark");
+    document.getElementById("input3").classList.add("dark");
 }
+
+
+
+let cookies="Nuestro sitio web utiliza cookies. Al dar click en aceptar,permites su uso.";
+alert(cookies);
+
+let tratamientoInfo="La informacion recopilada de esta pagina no sera compartida con terceros. Solo se usara para contactarnos contigo.";
+alert (tratamientoInfo);
